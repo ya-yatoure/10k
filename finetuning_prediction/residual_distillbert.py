e26060de509aadb2752459a61b17ac139f1de2d0
ya-yatoure <115236902+ya-yatoure@users.noreply.github.com>
2023-06-23 12:56:13 +0100

    Update residual_distillbert.py

--- finetuning_prediction/residual_distillbert.py
+++ finetuning_prediction/residual_distillbert.py
@@ -40,15 +40,29 @@ df['ER_1_residuals'] = df['ER_1'] - initial_regressor.predict(df[['lev', 'logEMV
 unique_companies = df['cik'].unique()
 train_companies, test_companies = train_test_split(unique_companies, test_size=0.2)
 
+# Here we split the train_companies into train and validation
+train_companies, val_companies = train_test_split(train_companies, test_size=0.2)
+
 train_df = df[df['cik'].isin(train_companies)]
+val_df = df[df['cik'].isin(val_companies)]  # New validation data frame
 test_df = df[df['cik'].isin(test_companies)]
-
-
 
 # For Train and Test set:
 # input IDs and attention masks as PyTorch tensors
 # structured data to a PyTorch tensor
 # abnormal returns variable as a PyTorch tensor
+# Create data loaders
+batch_size = 16
+
+# Encode the validation data
+val_encodings = distilbert_tokenizer(list(val_df['text']), truncation=True, padding=True)
+val_input_ids = torch.tensor(val_encodings['input_ids'])
+val_attention_mask = torch.tensor(val_encodings['attention_mask'])
+val_target = torch.tensor(val_df['ER_1_residuals'].values, dtype=torch.float)
+
+val_data = TensorDataset(val_input_ids, val_attention_mask, val_target)
+val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)  # New validation data loader
+
 train_encodings = distilbert_tokenizer(list(train_df['text']), truncation=True, padding=True)
 train_input_ids = torch.tensor(train_encodings['input_ids'])
 train_attention_mask = torch.tensor(train_encodings['attention_mask'])
@@ -62,10 +76,6 @@ test_target = torch.tensor(test_df['ER_1_residuals'].values, dtype=torch.float)
 train_data = TensorDataset(train_input_ids, train_attention_mask, train_target)
 test_data = TensorDataset(test_input_ids, test_attention_mask, test_target)
 
-
-# Create data loaders
-batch_size = 16
-
 train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
@@ -74,7 +84,13 @@ class DistilBERTRegressor(nn.Module):
         super(DistilBERTRegressor, self).__init__()
         self.distilbert = DistilBertModel.from_pretrained("distilbert-base-uncased")
         self.dropout = nn.Dropout(0.2)
-        self.ffnet = nn.Linear(self.distilbert.config.dim, 1)  # ffnet
+        
+        # combined layers after getting embeddings from DistilBERT
+        self.combined_layer = nn.Sequential(
+            nn.Linear(self.distilbert.config.dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
 
     def forward(self, input_ids, attention_mask):
         # get embeddings from DistilBERT model
@@ -84,9 +100,11 @@ class DistilBERTRegressor(nn.Module):
         # pool over sequence dimension
         embeddings = embeddings.mean(dim=1)
 
-        # pass pooled embeddings through the ffnet
-        output = self.ffnet(embeddings)
+        # pass pooled embeddings through the combined layers
+        output = self.combined_layer(embeddings)
+        
         return output.squeeze(-1)
+
 
 
 model = DistilBERTRegressor()
@@ -102,42 +120,69 @@ optimizer = optim.Adam(model.parameters())
 # Training loop
 n_epochs = 25  # adjust as needed
 
-for epoch in range(n_epochs):
-    model.train()  # turn on training mode
-    total_loss = 0
-    print("Starting epoch", epoch)
+# Initialize early stopping variables
+patience = 5
+best_val_loss = float('inf')
+epochs_no_improve = 0
 
+# Training loop
+for epoch in range(n_epochs):
+    model.train()
+    total_loss = 0
+
+    # Actual training part
     for i, batch in enumerate(train_loader):
         # Get data
         input_ids, attention_mask, targets = batch
-
         # Move data to the GPU if available
         input_ids = input_ids.to(device)
         attention_mask = attention_mask.to(device)
         targets = targets.to(device)
-
-        # Zero the gradients
+        # Clear the gradients
         optimizer.zero_grad()
-
-        # Perform forward pass
+        # Make predictions
         outputs = model(input_ids, attention_mask)
-
         # Compute loss
         loss = criterion(outputs, targets)
-
-        # Perform backward pass
+        # Perform backpropagation
         loss.backward()
-
-        # Perform optimization
+        # Update the weights
         optimizer.step()
-
-        # Print intermediate loss
-        if i % 100 == 0:  # print every 100 batches
-            print("Batch: {}, Loss: {}".format(i, loss.item()))
 
         total_loss += loss.item()
 
-    print("Epoch: {}, Loss: {}".format(epoch, total_loss))
+    print("Epoch: {}, Training Loss: {}".format(epoch, total_loss / len(train_loader)))
+
+    # After each epoch of training, evaluate on the validation set
+    model.eval()
+    val_loss = 0
+    with torch.no_grad():
+        for i, batch in enumerate(val_loader):
+            # Get data
+            input_ids, attention_mask, targets = batch
+            # Move data to the GPU if available
+            input_ids = input_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            targets = targets.to(device)
+            # Make predictions
+            outputs = model(input_ids, attention_mask)
+            # Compute loss
+            loss = criterion(outputs, targets)
+            val_loss += loss.item()
+
+        val_loss = val_loss / len(val_loader)  # average loss
+
+    print("Epoch: {}, Validation Loss: {}".format(epoch, val_loss))
+
+    # Early stopping
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        epochs_no_improve = 0
+    else:
+        epochs_no_improve += 1
+        if epochs_no_improve == patience:
+            print("Early stopping!")
+            break  # break the training loop
 
 # switch to evaluation mode
 model.eval()
