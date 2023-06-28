155ab6fbb62814099bf0c528cb2c219ddec7714b
ya-yatoure <115236902+ya-yatoure@users.noreply.github.com>
2023-06-28 17:05:41 +0100

    Update daulinput_gpt2.py

--- finetuning_prediction/daulinput_gpt2.py
+++ finetuning_prediction/daulinput_gpt2.py
@@ -13,151 +13,155 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 
-# Initialize tokenizer and add padding token
+# Define the hyperparameters
+BATCH_SIZE = 16
+EPOCHS = 30
+LEARNING_RATE = 1e-3
+LEARNING_RATE_GPT2 = 1e-5
+WEIGHT_DECAY = 1e-5
+DATASET_FRACTION = 0.001
+TRAIN_TEST_SPLIT_RATIO = 0.2
+
 tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
 tokenizer.pad_token = tokenizer.eos_token
-
 scaler = StandardScaler()
 
 df = pd.read_csv("../Data/text_covars_to1024_2019_sample.csv")
-dataset_fraction = 1.0  # use frac of data
 
-# Sample dataset_fraction of the data
-df = df.sample(frac=dataset_fraction)
+# Use defined hyperparameter
+df = df.sample(frac=DATASET_FRACTION)
 
-# Continue with  train/test split
-# group by companies when test/train splitting so we dont have companies that appear in test and trainset
+# one hot encoding 'naics2'
+df = pd.get_dummies(df, columns=['naics2'])
+
+# group by companies when test/train splitting so we don't have companies that appear in both test and train sets
 unique_companies = df['cik'].unique()
-train_companies, test_companies = train_test_split(unique_companies, test_size=0.2)
-
-train_df = df[df['cik'].isin(train_companies)]
-test_df = df[df['cik'].isin(test_companies)]
-
-# group by companies when test/train splitting so we dont have companies that appear in test and trainset
-unique_companies = df['cik'].unique()
-train_companies, test_companies = train_test_split(unique_companies, test_size=0.2)
+train_companies, test_companies = train_test_split(unique_companies, test_size=TRAIN_TEST_SPLIT_RATIO)
 
 train_df = df[df['cik'].isin(train_companies)]
 test_df = df[df['cik'].isin(test_companies)]
 encodings = tokenizer(list(df['text']), truncation=True, padding=True)
 
+# structured columns now include the one-hot-encoded 'naics2' columns as well
+structured_columns = ['lev', 'logEMV'] + [col for col in df.columns if 'naics2' in col]
 
-# For Train and Test set:
-# input IDs and attention masks as PyTorch tensors
-# structured data to a PyTorch tensor
-# abnormal returns variable as a PyTorch tensor
-
+# For Train and Test set
 train_encodings = tokenizer(list(train_df['text']), truncation=True, padding=True)
 train_input_ids = torch.tensor(train_encodings['input_ids'])
 train_attention_mask = torch.tensor(train_encodings['attention_mask'])
-train_structured_data = scaler.fit_transform(train_df[['lev', 'logEMV', 'naics2']])
+
+# Only scale 'lev' and 'logEMV'
+train_structured_data_to_scale = scaler.fit_transform(train_df[['lev', 'logEMV']])
+train_structured_data_one_hot = train_df[[col for col in train_df.columns if 'naics2' in col]].values
+train_structured_data = np.concatenate((train_structured_data_to_scale, train_structured_data_one_hot), axis=1)
 train_structured_data = torch.tensor(train_structured_data, dtype=torch.float)
+
 train_target = torch.tensor(train_df['ER_1'].values, dtype=torch.float)
 
 test_encodings = tokenizer(list(test_df['text']), truncation=True, padding=True)
 test_input_ids = torch.tensor(test_encodings['input_ids'])
 test_attention_mask = torch.tensor(test_encodings['attention_mask'])
-test_structured_data = scaler.transform(test_df[['lev', 'logEMV', 'naics2']])
+
+# Only scale 'lev' and 'logEMV'
+test_structured_data_to_scale = scaler.transform(test_df[['lev', 'logEMV']])
+test_structured_data_one_hot = test_df[[col for col in test_df.columns if 'naics2' in col]].values
+test_structured_data = np.concatenate((test_structured_data_to_scale, test_structured_data_one_hot), axis=1)
 test_structured_data = torch.tensor(test_structured_data, dtype=torch.float)
 
 test_target = torch.tensor(test_df['ER_1'].values, dtype=torch.float)
 
-
 train_data = TensorDataset(train_input_ids, train_attention_mask, train_structured_data, train_target)
 test_data = TensorDataset(test_input_ids, test_attention_mask, test_structured_data, test_target)
 
-# Maintain a separate list of 'cik' values
 test_cik = test_df['cik'].values.tolist()
 
-train_dataloader = DataLoader(train_data, batch_size=16)
-test_dataloader = DataLoader(test_data, batch_size=16)
-
-
-
-# split train_data into training set and validation set
-train_size = int(0.8 * len(train_data))  # 80% for training
-val_size = len(train_data) - train_size  # the rest for validation
+train_size = int(0.8 * len(train_data))
+val_size = len(train_data) - train_size
 train_data, val_data = torch.utils.data.random_split(train_data, [train_size, val_size])
 
-# DataLoaders for training and validation sets
-train_dataloader = DataLoader(train_data, batch_size=16, shuffle=True)
-val_dataloader = DataLoader(val_data, batch_size=16)
+# Use defined hyperparameters
+train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+val_dataloader = DataLoader(val_data, batch_size=BATCH_SIZE)
+test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE)
 
 
+# DEFINE MODEL ARCHITECTURE
+class GPT2WithStructuredData(nn.Module):
+    def __init__(self, num_structured_features, context_vector_dim):
+        super(GPT2WithStructuredData, self).__init__()
 
-class DualInputModel(nn.Module):
-    def __init__(self, num_structured_features, text_embedding_dim):
-        super(DualInputModel, self).__init__()
-        
-        # GPT-2 model for the text data
+        # GPT2 model for the text data (unfrozen)
         self.gpt2 = GPT2Model.from_pretrained('gpt2')
-        
-        # Freeze GPT-2
-        for param in self.gpt2.parameters():
-            param.requires_grad = False
+
+        # context vector plus structured feature NN output
+        combined_dim = context_vector_dim + 8
 
         # A feed-forward neural network for the structured data
         self.ffnn = nn.Sequential(
-            nn.Linear(num_structured_features, 6),
+            nn.Linear(num_structured_features, 24),
+            nn.BatchNorm1d(24),
+            nn.Dropout(0.25),
             nn.ReLU(),
-            nn.Linear(6, 6)
+            nn.Linear(24, 8),
+            nn.BatchNorm1d(8)
         )
 
-        combined_dim = text_embedding_dim + 6
-        
-        # layers after combination
         self.combined_layer = nn.Sequential(
             nn.Linear(combined_dim, 64),
+            nn.BatchNorm1d(64),
+            nn.Dropout(0.25),
             nn.ReLU(),
             nn.Linear(64, 1)
         )
 
-
     def forward(self, input_ids, attention_mask, structured_data):
-        # Pass the text data through GPT-2
+        # Pass the text data through GPT2
         gpt2_output = self.gpt2(input_ids=input_ids, attention_mask=attention_mask)
-        text_embeddings = gpt2_output.last_hidden_state.mean(dim=1)  # Average the sequence dimension
+        
+        # Instead of global attention mechanism, take the average of hidden state embeddings
+        context_vector = gpt2_output.last_hidden_state.mean(dim=1)
 
         # Pass the structured data through the feed-forward neural network
         structured_embeddings = self.ffnn(structured_data)
 
-        # Concatenate the text embeddings and structured embeddings
-        combined = torch.cat((text_embeddings, structured_embeddings), dim=1)
+        # Concatenate the context vector and structured embeddings
+        combined = torch.cat((context_vector, structured_embeddings), dim=1)
 
-        # Pass the combined embeddings through the combined layer
+        # Pass the combined data through the combined layer
         output = self.combined_layer(combined)
 
         return output
-
 
 
 # Check if CUDA is available, otherwise use CPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Define the model
-model = DualInputModel(num_structured_features=3, text_embedding_dim=768).to(device)
+model = GPT2WithStructuredData(num_structured_features=len(structured_columns), context_vector_dim=768).to(device)
+
+# Freeze the GPT2 weights
+for param in model.gpt2.parameters():
+    param.requires_grad = False
 
 # Separately get the parameters of the GPT2 model and the rest of your model
 gpt2_params = model.gpt2.parameters()
 other_params = [p for p in model.parameters() if p not in gpt2_params]
 
-# Use a smaller learning rate for the DistilBERT parameters
+# Use defined hyperparameters
 optimizer = optim.Adam([
-    {'params': gpt2_params, 'lr': 1e-5},
-    {'params': other_params, 'lr': 1e-3}
+    {'params': gpt2_params, 'lr': LEARNING_RATE_GPT2, 'weight_decay': WEIGHT_DECAY},
+    {'params': other_params, 'lr': LEARNING_RATE, 'weight_decay': WEIGHT_DECAY}
 ])
 
 loss_fn = nn.MSELoss()
 
-epochs = 30
-batch_size = 16
 train_losses, val_losses = [], []
 
 # initialize these for early stopping
 best_val_loss = float('inf')
 no_improve_epochs = 0
 
-for epoch in range(epochs):
+for epoch in range(EPOCHS):
     for dataloader, is_training in [(train_dataloader, True), (val_dataloader, False)]:
         total_loss = total_samples = 0
         model.train(is_training)
@@ -195,8 +199,6 @@ for epoch in range(epochs):
     if no_improve_epochs >= 10:
         break  # break out from epoch loop as well
 
-
-
 # Generate predictions after all epochs
 model.eval()
 
@@ -207,7 +209,7 @@ actuals = []
 with torch.no_grad():
     for i, batch in enumerate(test_dataloader):
         test_inputs, test_masks, test_structured, targets = (t.to(device) for t in batch)
-        company_ids = test_cik[i * batch_size: (i + 1) * batch_size]  # Adjust indices based on batch size
+        company_ids = test_cik[i * BATCH_SIZE: (i + 1) * BATCH_SIZE]  # Adjust indices based on batch size
         outputs = model(test_inputs, test_masks, test_structured)
         outputs = outputs.cpu().detach().numpy()
 
@@ -228,12 +230,4 @@ actuals = np.array(actuals)
 
 r2 = r2_score(actuals, predictions)
 print(f"R^2 score: {r2}")
-
-plt.plot(train_losses, label='Training Loss per observation')
-plt.plot(val_losses, label='Validation Loss per observation')
-plt.xlabel('Epoch')
-plt.ylabel('Loss per observation')
-plt.legend()
-plt.show()
-
 
