239ce82762faee51ae8510fffe09b9c519f0835c
ya-yatoure <115236902+ya-yatoure@users.noreply.github.com>
2023-07-13 12:12:02 +0100

    Update residual.py

--- finetuning_prediction/residual.py
+++ finetuning_prediction/residual.py
@@ -208,15 +208,8 @@ else:
             total_loss += loss.item()
             loss.backward()
             optimizer.step()
-        if epoch % 2 == 0:  # Print losses every 2 epochs
-            val_loss = 0.0
-            model.eval()
-        with torch.no_grad():
-            for batch in dataloaders['val']:
-                input_ids, attention_mask, targets = [b.to(device) for b in batch]
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-                loss = loss_func(outputs.view(-1), targets.view(-1))
-                val_loss += loss.item()
+
+    
     model.eval()
     predictions = []
     true_values = []
