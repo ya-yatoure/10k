cc805e154236e2a906bfb2f3826b95060b912779
ya-yatoure <115236902+ya-yatoure@users.noreply.github.com>
2023-07-13 12:32:03 +0100

    Update residual.py

--- finetuning_prediction/residual.py
+++ finetuning_prediction/residual.py
@@ -19,7 +19,7 @@ import pandas as pd
 LEARNING_RATE = 0.01
 EPOCHS = 10
 BATCH_SIZE = 16
-DATASET_FRACTION = 0.02
+DATASET_FRACTION = 0.5
 TRAIN_TEST_SPLIT_RATIO = 0.2
 GRID_SEARCH = True  # Set this to True if you want to perform grid search
 
